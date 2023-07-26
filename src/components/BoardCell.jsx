42b6c572b2b39449cf5438bd53a7bd3fc5fc7c5f
Vanessa Depraute <vanessadepraute@gmail.com>
2023-07-27 01:18:24 +0200

    design puyo

--- src/components/BoardCell.jsx
+++ src/components/BoardCell.jsx
@@ -4,13 +4,9 @@ const BoardCell = ({ cell }) => (
   <div className={`BoardCell ${cell.className}`}>
     <div className="Sparkle">
       <div class="eye">
-        <div class="leftEye">
-          <div class="leftPupil"></div>
-        </div>
+        <div class="leftEye"></div>
 
-        <div class="rightEye">
-          <div class="rightPupil"></div>
-        </div>
+        <div class="rightEye"></div>
       </div>
     </div>
   </div>
